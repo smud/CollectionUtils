f109ca5a387979bef3b6f04b4833a5e4c8da48fb
Andrey Fidrya <af@zabiyaka.com>
2016-11-19 23:30:58 +0300

    Initial commit

---
+++ Tests/CollectionUtilsTests/CollectionUtilsTests.swift
@@ -0,0 +1,17 @@
+import XCTest
+@testable import CollectionUtils
+
+class CollectionUtilsTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //XCTAssertEqual(CollectionUtils().text, "Hello, World!")
+    }
+
+
+    static var allTests : [(String, (CollectionUtilsTests) -> () throws -> Void)] {
+        return [
+            ("testExample", testExample),
+        ]
+    }
+}
