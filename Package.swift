9e05381e5b1c87d385c5e654d69295fabc1751ec
Andrey Fidrya <af@zabiyaka.com>
2018-01-21 03:21:04 +0300

    Swift 4 migration

--- Package.swift
+++ Package.swift
@@ -1,5 +1,14 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "CollectionUtils"
+    name: "CollectionUtils",
+    products: [
+        .library(name: "CollectionUtils", targets: ["CollectionUtils"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "CollectionUtils"),
+    ]
 )
